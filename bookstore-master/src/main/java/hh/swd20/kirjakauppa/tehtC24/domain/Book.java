136f5d14f232bdedc6143c165035a926b04ba7dc
bgh331 <58461569+bgh331@users.noreply.github.com>
2020-02-06 21:29:00 +0200

    Add files via upload

---
+++ bookstore-master/src/main/java/hh/swd20/kirjakauppa/tehtC24/domain/Book.java
@@ -0,0 +1,70 @@
+package hh.swd20.kirjakauppa.tehtC24.domain;
+
+public class Book {
+
+    private String title, author, year, ISBN;
+    private double price;
+
+    public Book (String title, String author, String year, String ISBN, double price) {
+
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.ISBN = ISBN;
+        this.price = price;
+
+    }
+
+	public Book() {
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", ISBN=" + ISBN + ", price=" + price
+				+ "]";
+	}
+    
+    
+
+}
